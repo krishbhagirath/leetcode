b948cb997137fb863759a8b7e2797d84b1aec381
krishbhagirath <krishh.b20@gmail.com>
2025-08-15 00:46:36 -0400

    twoSum (easy)

---
+++ arrays/two_sum.py
@@ -0,0 +1,39 @@
+from typing import List
+
+# Submission 1 - Brute Force (nested loops)
+def twoSumBrute(nums: List[int], target: int):
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i,j]
+
+# Submission 2 - More Optimized (DOES NOT WORK - CHECKS SAME INDICES AGAINST ITSELF)
+def twoSumWrong(nums: List[int], target: int):
+    for i in range(len(nums)):
+        remaining = target - nums[i]
+        
+        if remaining in nums:
+            return [i, nums.index(remaining)]
+
+# Submission 3 - Optimal (using dictionary)
+def twoSumOptimal(nums: List[int], target: int):
+    seen = {} # dictionary (value/index pairs)
+
+    for i, x in enumerate(nums):
+        need = target - x # 3
+        if need in seen: # checks the keys of the dictionary for 'need'
+            return [seen[need], i] # if seen, return i (current index) and seen[need] (index of seen number)
+        seen[x] = i
+
+    # NOTE
+    # list: when you use 'in', it looks through the VALUES of the list (ex: nums[i] provides value of nums at index i)
+    # dictionaries: when you use 'in', it looks through the KEYS of the list (ex: seen[need] provides the key at which value 'need' is found in dictionary)
+
+
+print("Brute Force Outputs:")
+print(twoSumBrute([3,4,5,6], 7))
+print(twoSumBrute([4,5,6], 10))
+print("---------------------------")
+print("Optimized Outputs:")
+print(twoSumOptimal([3,4,5,6], 7))
+print(twoSumOptimal([4,5,6], 10))
